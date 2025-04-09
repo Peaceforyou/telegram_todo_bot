dac3773f3cad027b7d67bdbe2694dd029716df66
Peaceforyou <kolibrimax05@mail.ru>
2025-04-09 22:29:36 +0300

    Added database models

--- models.py
+++ models.py
@@ -1,6 +1,28 @@
-from sqlalchemy.orm import mapped_column
-from sqlalchemy import Column, Integer, String, ForeignKey 
 from database import Base
+from sqlalchemy.orm import Mapped,mapped_column
+from sqlalchemy import BIGINT,TEXT,ForeignKey,MetaData
+
+
+metadata_obj = MetaData()
 
 
 
+class people(Base):
+    __tablename__ = 'people'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    users_id: Mapped[BIGINT]
+    
+    
+class tasks(Base):
+    __tablename__ = 'tasks'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)    
+    users_id: Mapped[BIGINT] = mapped_column(ForeignKey('people.id',ondelete='CASCADE'))
+    description: Mapped[TEXT]
+    
+    
+class feedbacks2(Base):
+    __tablename__ = 'feedbacks2'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
